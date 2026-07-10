4131469219efb2df830da42206da767df9458479
soumyadip-bairagi-pwc <soumyadip.bairagi@pwc.com>
2026-07-10 10:52:46 +0530

    Edited Text

--- src/Components/Notification/index.js
+++ src/Components/Notification/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './index.css'; // Import your CSS file for styles
 
 const Notification = () => {
-  const message = "SUJOG Portal will be undergoing server maintenance from 8:00 PM on 10th July 2026 to 8:00 PM on 11th July 2026. During this period, the portal will be temporarily unavailable.";
+  const message = "SUJOG Portal will be undergoing server maintenance at the State Data Center from 8:00 PM on 10th July 2026 to 8:00 PM on 11th July 2026. During this period, the portal will be temporarily unavailable for any business transaction.";
 
   return (
     <div className="notification-bar">
