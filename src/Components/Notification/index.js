9a86b4fbf1866fbf8e54b13b80ca924ebfb5c970
dsatapathy <107023068+dsatapathy@users.noreply.github.com>
2026-08-13 18:17:35 +0530

    Revert "Enable Notification"

--- src/Components/Notification/index.js
+++ src/Components/Notification/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './index.css'; // Import your CSS file for styles
 
 const Notification = () => {
-  const message = "SUJOG Portal will be undergoing server maintenance at the State Data Center from 7:00 PM on 13th August 2026 to 1:00 AM on 14th August 2026. During this period, the portal will be temporarily unavailable for business transaction.";
+  const message = "SUJOG Portal will be undergoing server maintenance at the State Data Centre from 8:00 PM on 10th July 2026 to 8:00 PM on 11th July 2026. During this period, the portal will be temporarily unavailable for any business transaction. ";
 
   return (
     <div className="notification-bar">
